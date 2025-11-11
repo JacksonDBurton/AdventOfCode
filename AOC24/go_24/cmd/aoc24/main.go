3d57f901b01c1a74e0a1b0d1024aae59e8acc6c7
JacksonDBurton <jdburton0@outlook.com>
2025-11-11 13:21:53 -0500

    #24: Add full solution for D1Q1

---
+++ AOC24/go_24/cmd/aoc24/main.go
@@ -0,0 +1,18 @@
+// Package aoc24 is the main package for go_24 AOC project
+package aoc24
+
+import (
+	"fmt"
+	"os"
+
+	"AOC24/internal/day1"
+)
+
+func main() {
+	f, err := os.Open("../../internal/day1/day1_historian_hysteria_input.txt")
+	if err != nil {
+		panic("Open day1 historian hysteria input failed")
+	}
+
+	fmt.Printf("Day1 Q1: %v", day1.SolveQuestion(f))
+}
--- AOC24/go_24/internal/day1/day1_historian_hysteria.go
+++ AOC24/go_24/internal/day1/day1_historian_hysteria.go
@@ -3,8 +3,48 @@
 package day1
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"slices"
+	"strconv"
+	"strings"
 )
+
+func SolveQuestion(questionFile *os.File) (answer string) {
+	answerList := []int{}
+	scanner := bufio.NewScanner(questionFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		pair := strings.Split(line, " ")
+		if len(pair) != 2 {
+			panic("Pair length not 2")
+		}
+
+		xInt, err := strconv.Atoi(pair[0])
+		if err != nil {
+			panic(fmt.Sprintf("Failed to convert x: %v", pair[0]))
+		}
+		yInt, err := strconv.Atoi(pair[1])
+		if err != nil {
+			panic(fmt.Sprintf("Failed to convert y: %v", pair[1]))
+		}
+		x := splitDigits(xInt)
+		y := splitDigits(yInt)
+		answerList = append(answerList, SolveListDistance(x, y))
+	}
+
+	return strings.Join(strings.Split(fmt.Sprint(answerList), ", "), "")
+}
+
+func splitDigits(n int) (digits []int) {
+	for n > 0 {
+		dig := n % 10
+		n /= 10
+		digits = append(digits, dig)
+	}
+	return
+}
 
 func SolveListDistance(x []int, y []int) (totalDistance int) {
 	slices.Sort(x)
